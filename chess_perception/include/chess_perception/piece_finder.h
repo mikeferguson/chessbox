e4cd8726ce5808a53aa2b4013bc2f09215158503
Mike Ferguson <mike@vanadiumlabs.com>
2013-03-15 00:15:19 -0700

    add new piece finder

---
+++ chess_perception/include/chess_perception/piece_finder.h
@@ -0,0 +1,56 @@
+/**
+
+Copyright (c) 2011-2013 Michael E. Ferguson.  All right reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+
+**/
+
+#ifndef PIECE_FINDER_H_
+#define PIECE_FINDER_H_
+
+#include <ros/ros.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/PointIndices.h>
+#include <pcl/segmentation/extract_polygonal_prism_data.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/segmentation/extract_clusters.h>
+
+/** \class PieceFinder
+ *  \brief Finds the pieces.
+ */
+class PieceFinder
+{
+  public:
+    PieceFinder();
+    virtual ~PieceFinder() {};
+
+    /** \brief Finds pieces on a table.
+     *
+     *  \returns number of pieces found.
+     */
+    int findPieces(pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloud,
+                   pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloud_hull,
+                   boost::shared_ptr< std::vector<pcl::PointXYZ> > piece_centroids,
+                   boost::shared_ptr< std::vector<double> > weights);
+    
+  private:
+    pcl::ExtractPolygonalPrismData<pcl::PointXYZRGB> extract_data_;
+    pcl::ExtractIndices<pcl::PointXYZRGB> extract_indices_;
+    pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> cluster_;
+};
+
+#endif
---
+++ chess_perception/src/piece_finder.cpp
@@ -0,0 +1,118 @@
+/**
+
+Copyright (c) 2011-2013 Michael E. Ferguson.  All right reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+
+**/
+
+#include <chess_perception/piece_finder.h>
+
+PieceFinder::PieceFinder()
+{
+    /* TODO read params from parameter server */
+
+    /* Setup extract polygonal prism data */
+    extract_data_.setHeightLimits(0.01, 0.2);
+
+    /* Setup extracting indices */
+    extract_indices_.setNegative(false);
+
+    /* Setup clustering */
+    cluster_.setClusterTolerance(0.01);
+    cluster_.setMinClusterSize(10);
+    cluster_.setMaxClusterSize(3000); // TODO: is this too low?
+
+}
+
+int PieceFinder::findPieces(pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloud,
+                            pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr table_hull,
+                            boost::shared_ptr< std::vector<pcl::PointXYZ> > pieces,
+                            boost::shared_ptr< std::vector<double> > weights)
+{
+    /* Extract points above convex hull */
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices());
+    extract_data_.setInputPlanarHull(table_hull);
+    extract_data_.setInputCloud(cloud);
+    extract_data_.segment(*inliers);
+
+    /* Extract points from indices found above */
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_pieces (new pcl::PointCloud<pcl::PointXYZRGB>);
+    extract_indices_.setInputCloud(cloud);
+    extract_indices_.setIndices(inliers);
+    extract_indices_.filter(*cloud_pieces);
+
+    /* Cluster */
+    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB>);
+    std::vector<pcl::PointIndices> clusters;
+    cluster_.setSearchMethod(tree);
+    cluster_.setInputCloud(cloud_pieces);
+    cluster_.extract(clusters);
+
+    /* Determine Weights */
+    weights.reset(new std::vector<double>);
+    pieces.reset(new std::vector<pcl::PointXYZ>);
+    for (size_t c = 0; c < clusters.size(); ++c)
+    {
+        /* find color/centroid of this cluster */
+        double x = 0; double y = 0; double z = 0; int color = 0;
+        for (size_t i = 0; i < clusters[c].indices.size(); i++)
+        {
+            int j = clusters[c].indices[i];
+            x += cloud_pieces->points[j].x;
+            y += cloud_pieces->points[j].y;
+            z += cloud_pieces->points[j].z;
+            unsigned char * rgb = (unsigned char *) &(cloud_pieces->points[j].rgb);
+            color += (rgb[0] + rgb[1] + rgb[2])/3;
+        }
+        x = x/clusters[c].indices.size();
+        y = y/clusters[c].indices.size();
+        z = z/clusters[c].indices.size();
+        color = color/clusters[c].indices.size();
+        
+        /* outputs */
+        pcl::PointXYZ p(x,y,z);
+        double weight = clusters[c].indices.size();
+        if(color < 100)
+            weight = -weight; // use negative numbers for black
+
+        /* check if cluster overlaps any other */
+        bool new_ = true;
+        for (size_t j = 0; j < pieces->size(); j++)
+        {
+            if( (fabs(p.x-(*pieces)[j].x) < 0.012) &&
+                (fabs(p.y-(*pieces)[j].y) < 0.012) )
+            {
+                double w1 = fabs((*weights)[j]);
+                double w2 = fabs(weight);
+                (*pieces)[j].x = ((*pieces)[j].x*w1 + p.x*w2)/(w1+w2);
+                (*pieces)[j].y = ((*pieces)[j].y*w1 + p.y*w2)/(w1+w2);
+                (*pieces)[j].z = ((*pieces)[j].z*w1 + p.z*w2)/(w1+w2);
+                (*weights)[j] += weight;
+                new_ = false;
+                break;
+            }
+        }
+    
+        /* add cluster if not overlapping with another */
+        if (new_){
+            // TODO: add check for too large to be a chess piece
+            pieces->push_back(p);
+            weights->push_back(weight);
+        }
+    }
+
+    return weights->size();
+}
