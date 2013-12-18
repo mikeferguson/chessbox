c1b9cdba518e222d24a3969768500344cb0b0471
Michael Ferguson <fergs@unboundedrobotics.com>
2013-12-17 22:52:08 -0800

    update api

--- moveit_utils/src/moveit_utils/geometry.py
+++ moveit_utils/src/moveit_utils/geometry.py
@@ -21,36 +21,50 @@
 from geometry_msgs.msg import *
 from tf.transformations import *
 
-## @brief Get a translation matrix from a geometry_msgs/Vector3
-## @param vector geometry_msgs/Vector3 to turn into matrix
-def matrix_from_vector(vector):
-    return translation_matrix((vector.x, vector.y, vector.z))
+## @brief Get a translation matrix from a geometry_msgs/Point
+## @param point geometry_msgs/Point to turn into matrix
+def matrix_from_point_msg(point):
+    return translation_matrix((point.x, point.y, point.z))
 
 ## @brief Get a rotation matrix from a geometry_msgs/Quaternion
 ## @param quaternion geometry_msgs/Quaternion to turn into matrix
-def matrix_from_quaternion(quaternion):
+def matrix_from_quaternion_msg(quaternion):
     q = [quaternion.x, quaternion.y, quaternion.z, quaternion.w]
     return quaternion_matrix(q)
 
 ## @brief Get a transformation matrix from a geometry_msgs/Pose
 ## @param pose geometry_msgs/Pose to turn into matrix
-def matrix_from_pose(pose):
-    t = matrix_from_vector(pose.position)
-    r = matrix_from_quaternion(pose.orientation)
+def matrix_from_pose_msg(pose):
+    t = matrix_from_point_msg(pose.position)
+    r = matrix_from_quaternion_msg(pose.orientation)
     return concatenate_matrices(t, r)
+
+## @brief Get a geometry_msgs/Point from a transformation matrix
+## @param transformation The matrix to convert to a point
+def point_msg_from_matrix(transformation):
+    msg = Point()
+    msg.x = transformation[0][3]
+    msg.y = transformation[1][3]
+    msg.z = transformation[2][3]
+    return msg
+
+## @brief Get a geometry_msgs/Quaternion from a transformation matrix
+## @param transformation The matrix to convert to a quaternion
+def quaternion_msg_from_matrix(transformation):
+    q = quaternion_from_matrix(transformation)
+    msg = Quaternion()
+    msg.x = q[0]
+    msg.y = q[1]
+    msg.z = q[2]
+    msg.w = q[3]
+    return msg
 
 ## @brief Get a geometry_msgs/Pose from a transformation matrix
 ## @param transformation The matrix to convert to a pose
-def pose_from_matrix(transformation):
-    p = Pose()
-    p.position.x = transformation[0][3]
-    p.position.y = transformation[1][3]
-    p.position.z = transformation[2][3]
-    q = quaternion_from_matrix(transformation)
-    p.orientation.x = q[0]
-    p.orientation.y = q[1]
-    p.orientation.z = q[2]
-    p.orientation.w = q[3]
+def pose_msg_from_matrix(transformation):
+    msg = Pose()
+    msg.position = point_msg_from_matrix(transformation)
+    msg.orientation = quaternion_msg_from_matrix(transformation)
     return p
 
 ## @brief Translate a geometry_msgs/Pose
@@ -58,8 +72,8 @@ def pose_from_matrix(transformation):
 ## @param x The displacement in X coordinate axis
 ## @param y The displacement in Y coordinate axis
 ## @param z The displacement in Z coordinate axis
-def translate_pose(pose, x, y, z):
-    initial = matrix_from_pose(pose)
+def translate_pose_msg(pose, x, y, z):
+    initial = matrix_from_pose_msg(pose)
     transform = translation_matrix((x,y,z))
     return pose_from_matrix(concatenate_matrices(initial, transform))
 
@@ -68,8 +82,8 @@ def translate_pose(pose, x, y, z):
 ## @param r The roll
 ## @param p The pitch
 ## @param y The yaw
-def rotate_pose_by_euler_angles(pose, r, p, y):
-    initial = matrix_from_pose(pose)
+def rotate_pose_msg_by_euler_angles(pose, r, p, y):
+    initial = matrix_from_pose_msg(pose)
     transform = quaternion_matrix(quaternion_from_euler(r, p, y))
-    return pose_from_matrix(concatenate_matrices(initial, transform))
+    return pose_msg_from_matrix(concatenate_matrices(initial, transform))
 
